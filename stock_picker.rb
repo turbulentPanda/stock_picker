7b5eade5816f2bb8b3a373d1feb4312b04005a8c
Christopher Marchant <cmarch240@gmail.com>
2021-12-02 16:36:40 -0600

    Refactored code to make it much cleaner and simpler

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,40 @@
-def max_difference(number, array_of_numbers)
-  max_number = array_of_numbers.max
-  {[number, max_number] => max_number - number}
-end
+def pick_days(array_of_prices)
+  min_price = array_of_prices[0]
+  min_price_index = 0
 
-  
-def create_array_of_max_differences(stock_prices_array)
-  stock_prices_array.filter_map do |price|
-    price_index = stock_prices_array.index(price)
-    unless price == stock_prices_array.last
-      max_difference(price, stock_prices_array[price_index+1..stock_prices_array.length])
+  profit = 0
+  days = [0, 0]
+
+  array_of_prices.each_with_index do |price, index|
+    if price < min_price
+      min_price = price
+      min_price_index = index
+    end
+
+    if price - min_price > profit
+      profit = price - min_price
+      days = [min_price_index, index]
     end
   end
+
+    days
 end
+
+def stock_picker(array_of_prices)
+  days = pick_days(array_of_prices)
+
+  buying_day = days[0]
+  buying_price = array_of_prices[buying_day]
+
+  selling_day = days[1]
+  selling_price = array_of_prices[selling_day]
+
+  profit = selling_price - buying_price
+
+  "Buy on Day #{buying_day} for $#{buying_price} and sell on Day #{selling_day} for $#{selling_price}. Profit is $#{profit}."
+end
+
+
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
 
